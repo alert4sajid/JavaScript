81193b9b59517060998fe8f58cd1b745d5f966d0
Sajid Mehmood <alert4sajid@gmail.com>
2021-12-17 12:13:20 +0500

    Update Variables.js

---
+++ Variables.js
@@ -0,0 +1,9 @@
+var one = 1; // function scoped, can be changed in scope, available before declaration 
+let two = 2; // block scoped, can be changed in scope, available after declaration 
+const three = 3; // block scoped, can not be changed in scope, available after declaration 
+// const by default
+// let in loops
+
+// Declaring Variables
+var hello = "Hello";
+console.log(hello)
