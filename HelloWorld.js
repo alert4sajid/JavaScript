e2844e6e41aac4484fbbca49e90162f7fd71056e
Sajid Mehmood <alert4sajid@gmail.com>
2021-12-17 11:48:41 +0500

    2

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,10 @@
-console.log('Hello World 😎');
+//1
+console.log('Hello World 1')
+const hello = 'Hello';
+const world = 'World';
+//2
+console.log(hello,world,"2")
+//3
+console.log('%s %s 3',hello,world);
+//4
+console.log(`${hello}, ${world} 4`)
