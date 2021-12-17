482772e81f5fde12da1ff701b19915dde7366aa2
Sajid Mehmood <alert4sajid@gmail.com>
2021-12-17 15:18:58 +0500

    Update Strings.js

---
+++ Strings.js
@@ -0,0 +1,11 @@
+//String Concatenation
+let str1 = "Hello";
+let str2 = "World";
+console.log(str1 + str2);
+console.log(str1 + " new " + str2)
+
+// numbers 
+let num1 = 1;
+let num2 = "2";
+console.log(num1 + num2); //11 as string
+console.log(num1 + 1);    //2 as number
