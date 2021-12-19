bb922ee84de4f3b7fe3e7fe33ebb3613c31e41c2
Sajid Mehmood <alert4sajid@gmail.com>
2021-12-19 08:03:43 +0500

    1

--- Strings.js
+++ Strings.js
@@ -13,6 +13,26 @@ console.log(num1 + 1);    //2 as number
 
 console.log("-- Converting strings to numbers --");
 console.log(parseInt("100"));
-console.log(parseInt(num1));
+console.log(parseInt(num2));
 console.log(parseInt("ABCD"));
 console.log(parseInt("0xF")); //Hexadecimal
+
+let flo1 = "1.5"
+
+console.log(parseFloat("1.9"));
+console.log(parseFloat(flo1));
+console.log(parseFloat("ABCD"));
+
+//Numbers after special character are ignored
+console.log(parseInt("1.5"));
+console.log(parseInt("1 + 1"));
+
+// Using Template Literal
+
+console.log(parseInt(`${1 + 1}`));
+
+// Numbers to strings
+console.log("-- Converting numbers to strings --");
+console.log(num1.toString());
+console.log(flo1.toString());
+console.log((100).toString());
