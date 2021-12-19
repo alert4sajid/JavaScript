f93cfb60738b9d900d6176ca0f4b1234715e0f8b
Sajid Mehmood <alert4sajid@gmail.com>
2021-12-19 07:33:28 +0500

    1

--- Data Types.js
+++ Data Types.js
@@ -7,6 +7,27 @@ const person = {
     FirstName : "Sajid",
     LastName : "Mehmood"
 };
+function sayHello (person) {
+    console.log("Hello, " + person.FirstName)
+}
+
+//typeof
+console.log("-- typeof --");
+console.log(typeof people);
+console.log(typeof number);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof person);
+console.log(typeof sayHello);
+
+//instanceof
+console.log("-- instanceof --");
+console.log(people instanceof Array);
+console.log(number instanceof Number);
+console.log(str instanceof String);
+console.log(bool instanceof Boolean);
+console.log(person instanceof Object);
+console.log(sayHello instanceof Function);
 
 // Types can change
 let x = "Something"; // string
